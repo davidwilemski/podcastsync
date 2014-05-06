c973b9153241ba4cae46a268ea0ca91e06dfed62
David Wilemski <david@davidwilemski.com>
2014-05-06 14:33:15 -0400

    add a human-readable PodcastName string to dl args

--- shared/podcast/types.go
+++ shared/podcast/types.go
@@ -2,9 +2,10 @@ package podcast
 
 // PodcastDownloadArgs contains nessecary data for the service to download a file and find out which users are subscribed to the originating feed
 type PodcastDownloadArgs struct {
-	FeedID     int    // podcastsync internal id of the originating feed
-	FeedURL    string // Originating podcast feed's URL
-	PodcastURL string // URL with the media file's location
+	FeedID      int    // podcastsync internal id of the originating feed
+	FeedURL     string // Originating podcast feed's URL
+	PodcastName string // podcast name: name of the folder the podcast is put into
+	PodcastURL  string // URL with the media file's location
 }
 
 // PodcastDownloadReply response containing success or failure and corresponding message
