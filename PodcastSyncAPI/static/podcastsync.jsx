86398df2889c7d67c63e813e5823477fb187ee02
David Wilemski <david@davidwilemski.com>
2014-05-07 21:07:54 -0400

    clear input box when podcast is queued for download
    TODO: show some indication of valid or invalid URL

--- PodcastSyncAPI/static/podcastsync.jsx
+++ PodcastSyncAPI/static/podcastsync.jsx
@@ -1,30 +1,34 @@
 /** @jsx React.DOM */
 var DownloadPodcastFile = React.createClass({
+  getInitialState: function() {
+    return {url: ""};
+  },
     render: function() {
         return (
             <div className="download-file">
                 <form className="download-form" onSubmit={this.handleSubmit}>
-                    <input type="text" id="file-url"></input>
+                    <input type="text" id="file-url" value={this.state.url} onChange={this.handleInput}></input>
                     <button id="submit-file-download" type="submit" className="btn btn-primary">Send Podcast Episode To Dropbox!</button>
                 </form>
             </div>
         );
     },
-
     handleSubmit: function(e) {
-        var creds = this.props.dbclient.credentials();
+      var creds = this.props.dbclient.credentials();
 
-        $.ajax({
-          url: "/podcast/download",
-          dataType: 'json',
-          type: 'POST',
-          data: JSON.stringify({UID: creds.uid, AccessToken: creds.token, PodcastURL: $("#file-url").val()}),
-          success: function(data) {
-              alert("success! " + data)
-            // this.setState({data: data});
-          }.bind(this)
-        });
-        e.preventDefault();
-        return false;
+      $.ajax({
+        url: "/podcast/download",
+        type: 'POST',
+        data: JSON.stringify({UID: creds.uid, AccessToken: creds.token, PodcastURL: $("#file-url").val()}),
+      }).done(function(data) {
+        console.log(data)
+        window.alert("success! " + data);
+        this.setState({url: ""});
+      }.bind(this));
+
+      return false;
+    },
+    handleInput: function(e) {
+      this.setState({url: e.target.value})
     }
 });
